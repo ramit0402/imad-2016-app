1018dccde22e9378e492612ad2f77f97e08c5af4
ramit0402 <ramitlegend@gmail.com>
2016-11-22 23:07:13 +0530

    [imad-console] Updates js/main.js

--- js/main.js
+++ js/main.js
@@ -1,157 +1,138 @@
-/*----------------Prevent spaces in inputs ----------------*/
+function loadLoginForm () {
+    var loginHtml = `
+        <h3>Login/Register to unlock awesome features</h3>
+        <input type="text" id="username" placeholder="username" />
+        <input type="password" id="password" />
+        <br/><br/>
+        <input type="submit" id="login_btn" value="Login" />
+        <input type="submit" id="register_btn" value="Register" />
+        `;
+    document.getElementById('login_area').innerHTML = loginHtml;
+    
+    // Submit username/password to login
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
+    
+    var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+}
 
-$(document).ready(function(){		
-	$("#username").on("keydown", function (e) {
-	    return e.which !== 32;
-	});
-	$("#password").on("keydown", function (e) {
-	    return e.which !== 32;
-	});
-	$("#new_username").on("keydown", function (e) {
-	    return e.which !== 32;
-	});
-	$("#new_password").on("keydown", function (e) {
-	    return e.which !== 32;
-	});
-});
+function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+    `;
+}
 
-/*-------------------------------Request for the COUNTER API endpoint---------------------------------*/
+function loadLogin () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
+            }
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
 
-var counter = document.getElementById('counter');
-
-if (counter) {
-	var req = new XMLHttpRequest();	
-	req.onreadystatechange = function(){
-		if(req.readyState === XMLHttpRequest.DONE && req.status === 200) {
-			var count = req.responseText;
-			var counter = document.getElementById('counter');
-			counter.innerHTML = count.toString();
-		}
-	};
-	req.open("GET", window.location.protocol+"//"+window.location.host+"/counter", true);
-	req.send(null);
+function loadArticles () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!')
+            }
+        }
+    };
+    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
 }
 
 
+// The first thing to do is to check if the user is logged in!
+loadLogin();
 
-
-/*-------------------------------Login JS---------------------------------*/
-
-
-var register_btn = document.getElementById('register_btn');
-var login_btn = document.getElementById('login_btn');
-var logout_btn = document.getElementById('logout_btn');
-var username = document.getElementById('username');
-var password = document.getElementById('password');
-var new_username = document.getElementById('new_username');
-var new_password = document.getElementById('new_password');
-
-if (login_btn) {
-	login_btn.onclick = function(){
-			username = document.getElementById('username').value;
-		password = document.getElementById('password').value;
-		if (username === '' || password === '') {
-   					$("#username").attr("placeholder", "Required !");
-   					$("#password").attr("placeholder", "Required !");
-   				} else {
-					var req = new XMLHttpRequest();	
-					req.onreadystatechange = function(){
-						if(req.readyState === XMLHttpRequest.DONE){
-							// Do something
-							if (req.status === 200){
-								console.log('user logged in');
-								console.log(username);
-								console.log(password);
-								checklogin();
-							} else if (req.status === 403) {
-								alert('username/password is incorrect or does not exist!');
-							} else if (req.status === 500) {
-								alert('username does not exist!');
-							}
-							document.getElementById('login_btn').innerHTML="Log In";
-						}
-					};
-		
-		
-		req.open("POST", window.location.protocol+"//"+window.location.host+"/login", true);
-		req.setRequestHeader('Content-Type', 'application/json');
-		req.send(JSON.stringify({username: username, password: password}));
-		document.getElementById('login_btn').innerHTML="Logging In...";
-		}
-	}
-}
-
-
-
-if (register_btn) {
-	register_btn.onclick = function(){
-			new_username = document.getElementById('new_username').value;
-		new_password = document.getElementById('new_password').value;
-
-		var req = new XMLHttpRequest();
-		var req2 = new XMLHttpRequest();
-		if (new_username === '' || new_password === '') {
-   					$("#new_username").attr("placeholder", "Required !");
-   					$("#new_password").attr("placeholder", "Required !");
-   				} else {	
-				req.onreadystatechange = function(){
-					if(req.readyState === XMLHttpRequest.DONE){
-				// Do something
-				if (req.status === 200){
-							document.getElementById('username').value = document.getElementById('new_username').value;
-							document.getElementById('password').value = document.getElementById('new_password').value;
-							document.getElementById('new_username').value = '';
-							document.getElementById('new_password').value = '';
-							alert('New user created!');
-							$('#login_btn').click();
-				} else if (req.status === 403) {
-					alert('username/password is incorrect');
-				} else if (req.status === 500) {
-					console.log('username = '+new_username);
-					console.log('password = '+new_password);
-					alert('username already exists!');
-				}
-				document.getElementById('register_btn').innerHTML="Register";
-			}
-		};
-			console.log(new_username);
-		console.log(new_password);
-		req.open("POST", window.location.protocol+"//"+window.location.host+"/create-user", true);
-		req.setRequestHeader('Content-Type', 'application/json');
-		req.send(JSON.stringify({username: new_username, password: new_password}));
-		document.getElementById('register_btn').innerHTML="Creating user...";
-	}
-		
-	}
-}
-
-
-
-
-
-
-if (logout_btn) {
-	$('#logout_btn')[0].onclick = logout;
-	checklogin();
-}
-
-
-function logout(){
-	var req = new XMLHttpRequest();	
-		req.onreadystatechange = function(){
-			if(req.readyState === XMLHttpRequest.DONE){
-				// Do something
-				if (req.status === 200){
-					console.log('user logged out');
-					checklogin();
-				} else if (req.status === 403) {
-					alert('username/password is incorrect!');
-				} else if (req.status === 500) {
-					alert('username/password is incorrect!');
-				}
-			}
-		};
-		req.open("GET", window.location.protocol+"//"+window.location.host+"/logout", true);
-		req.send(null);
-}
-
+// Now this is something that we could have directly done on the server-side using templating too!
+loadArticles();
