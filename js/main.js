20acf8268c15818c038c8d379d6ca88e48058bd2
ramit0402 <ramitlegend@gmail.com>
2016-11-23 00:22:31 +0530

    [imad-console] Updates js/main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function loadLoginForm () {
     var loginHtml = `
-    <div class="col-md-4 col-md-offset-3" style="border:1px solid black" >
+    <div class="col-md-4 col-md-offset-3" style="border:1px solid black; border-radius:20px" >
         <h3>Login/Register</h3>
         <label for="username" class="col-sm-3 control-label">Username</label>
         <input type="text" class="form-control" id="username"  />
