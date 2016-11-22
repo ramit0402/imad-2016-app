6d2e24ac92474aa01dffcc826f2ecf78b1dd65e8
ramit0402 <ramitlegend@gmail.com>
2016-11-23 00:44:17 +0530

    [imad-console] Updates js/main.js

--- js/main.js
+++ js/main.js
@@ -88,33 +88,26 @@ function loadLoginForm () {
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
     var log = document.getElementById('login');
+    var footer = document.getElementById('footer');
     log.innerHTML = `
         <li><a id="login" href="/logout">Logout</a></li>
     `;
     loginArea.innerHTML=`
-    <div class="row">
-		<div class="col-md-12">
-			<div class="col-md-3">
-				<img src="images/article3.jpg" class="img-responsive" alt="Image Article 3" >
-			</div>
-			<div class="col-md-6 " style="margin-top:40px">
-				<a href="article/article-one"><h4>SHOPPING CASUAL WEAR AT CRAFTSVILLA</h4></a>
-				<p>    If you know me, you know how obsessed I am with palazzos and all things comfy. My search for them led me to Craftsvilla and to be honest I was pleasantly overwhelmed and spoilt for choice.  Since I only wanted to look for something casual, that you can wear every day at home or work, I was happy that there were categories for office wear and casuals, party wear, and festive wear. It makes it really easy to look for what you want exactly, instead of getting lost.....</p>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-12">
-			<div class="col-md-3 col-md-offset-3">
-				<img src="images/article4.jpg" class="img-responsive" alt="Image Article 4" >
-			</div>
-			<div class="col-md-6 " style="margin-top:40px">
-			    <a href="article/article-five"><h4>MAFIA III REVIEW - LOYALTY IS ROYALTY</h4></a>
-				<p>    The Mafia series has had a bit of a patchy history so far, with the first game being beloved and the second getting a tepid response largely thanks to its open world which served as little more than decoration. With some six years since the release of Mafia II we get yet another developer trying to bring series back to into the public eye. The story setup is that a young black man by the name of Lincoln Clay returns from serving in the Vietnam war, and once again finds himself caught up in his families problems involving the various local gangs......</p>
-			</div>
-		</div>
-	</div>
     `;
+    footer.innerHTML=`
+    <footer id="footer" style="margin-top:300px">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <div class="text-center center-block">
+            <p class="txt-railway">- BLOGS ONLINE -</p>
+            <a href="https://github.com/ramit0402"><i class="fa fa-github" style="font-size:36px"></i></a>
+            <a href="https://www.facebook.com/ramitlegend"><i class="fa fa-facebook-official" style="font-size:36px"></i></a>
+            <a href="https://plus.google.com/u/1/+RamitMuralidharan02"><i class="fa fa-google-plus-official" style="font-size:36px"></i></a>
+            <a href="https://twitter.com/ramit1996"><i class="fa fa-twitter" style="font-size:36px"></i></a>
+            <a href="mailto:ramit0402@gmail.com"><i class="fa fa-google" style="font-size:36px"></i></a>
+        </div>
+    </footer>
+    `;
+    
     
 }
 
