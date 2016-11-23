87c6afafcb68bfb592febe85220f6a14516dbfbd
ramit0402 <ramitlegend@gmail.com>
2016-11-23 11:33:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -181,9 +181,9 @@ function createTemplateSection(data) {
 	<!--CONATINER START-->
 	<div class="container col-md-10 col-md-offset-1" style="padding-top:200px;text-align:center">
     	${content}
-    	<footer id="footer" style="padding-top:200px">
+    	<footer id="footer" >
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <div class="text-center center-block"  style="padding-top:200px">
+            <div class="text-center center-block"  >
                 <p class="txt-railway" style="padding-top:200px">- BLOGS ONLINE -</p>
                 <a href="https://github.com/ramit0402"><i class="fa fa-github" style="font-size:36px"></i></a>
                 <a href="https://www.facebook.com/ramitlegend"><i class="fa fa-facebook-official" style="font-size:36px"></i></a>
